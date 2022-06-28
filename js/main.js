a16666616983d5cf084e7009b0af0c1d11a951a7
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-06-28 19:13:00 +0500

    Add change tabpanel view's part of JavaScript.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,70 @@
-// const elsSitenavItem = document.querySelectorAll('.index-features__item');
-// const elsSitenavLink = document.querySelectorAll('.index-features__feature-name');
+// Drop/Down FAQ
+const elsQuestionButton = document.querySelectorAll('.js-faq__questions-button');
+const elsAnswer = document.querySelectorAll('.js-faq__questions-answer');
+const elsArrow = document.querySelectorAll('.js-arrow');
 
 // Drop/Down FAQ
-const elQuestionButton = document.querySelectorAll('.js-faq__questions-button');
-const elAnswer = document.querySelectorAll('.js-faq__questions-answer');
-const elArrow = document.querySelectorAll('.js-arrow');
-const elItem = document.querySelectorAll('.js-faq__questions-item');
-
-
-// Drop/Down FAQ
-elQuestionButton.forEach(function (elButton, index) {
+elsQuestionButton.forEach(function (elButton, index) {
   elButton.addEventListener('click', function () {
-    elArrow[index].classList.toggle('arrow--open');
-    elAnswer[index].classList.toggle('faq__questions-answer--open');
+    elsArrow[index].classList.toggle('arrow--open');
+    elsAnswer[index].classList.toggle('faq__questions-answer--open');
+  });
+});
+
+
+
+const elsTab = document.querySelectorAll('.js-tabs__link');
+const elsTabItem = document.querySelectorAll('.js-tabs__item');
+const elsTabPanel = document.querySelectorAll('.js-tab-panel');
+
+function removeItemClass() {
+  elsTabItem.forEach(function(item){
+    item.classList.remove('tabs__item--active');
+  });
+}
+
+function removeActiveClass() {
+  elsTabPanel.forEach(function(element){
+    element.classList.remove('tab-panels__tab-panel--active');
+  });
+}
+
+elsTab.forEach(function (elTab, index) {
+
+  elTab.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    // console.log(elTab.href.split('#')[1]);
+
+    removeItemClass();
+    elsTabItem[index].classList.add('tabs__item--active');
+
+    removeActiveClass();
+    elsTabPanel[index].classList.add('tab-panels__tab-panel--active');
 
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const elsSitenavItem = document.querySelectorAll('.index-features__item');
+// const elsSitenavLink = document.querySelectorAll('.index-features__feature-name');
 
 // elsSitenavLink.forEach(function (elLink) {
 //   elLink.addEventListener('click', function (evt) {
