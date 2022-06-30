cd53669f36d6155b4d891aa7c1194a1a0328ea1d
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-06-30 17:00:55 +0500

    Add modifiers and fix code structure in JS file.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
-// Drop/Down FAQ
-const elsAccordionToggler = document.querySelectorAll('.js-accordion-toggler');
-const elsAccordionContent = document.querySelectorAll('.js-accordion-content');
+const modifieries = {
+  // Drop/Down Accordion
+  accordionActive: 'accordion__button--open',
+  toggleActive: 'accordion__content--open',
 
-// Drop/Down FAQ
-elsAccordionToggler.forEach(function (elToggler, index) {
-  elToggler.addEventListener('click', function () {
-    elsAccordionToggler[index].classList.toggle('accordion__button--open');
-    elsAccordionContent[index].classList.toggle('accordion__content--open');
-  });
-});
+  // TabPanels
+  tabActive: 'tabs__item--active',
+  panelActive: 'tab-panels__tab-panel--active'
+};
 
-
-
+// TabPanels
 const elsTab = document.querySelectorAll('.js-tabs__link');
 const elsTabItem = document.querySelectorAll('.js-tabs__item');
 const elsTabPanel = document.querySelectorAll('.js-tab-panel');
 
+// Drop/Down Accordion
+const elsAccordionToggler = document.querySelectorAll('.js-accordion-toggler');
+const elsAccordionContent = document.querySelectorAll('.js-accordion-content');
+
+
+// TabPanels
 function removeItemClass() {
   elsTabItem.forEach(function(item){
-    item.classList.remove('tabs__item--active');
+    item.classList.remove(modifieries.tabActive);
   });
 }
 
 function removeActiveClass() {
   elsTabPanel.forEach(function(element){
-    element.classList.remove('tab-panels__tab-panel--active');
+    element.classList.remove(modifieries.panelActive);
   });
 }
 
@@ -35,10 +38,19 @@ elsTab.forEach(function (elTab, index) {
     // console.log(elTab.href.split('#')[1]);
 
     removeItemClass();
-    elsTabItem[index].classList.add('tabs__item--active');
+    elsTabItem[index].classList.add(modifieries.tabActive);
 
     removeActiveClass();
-    elsTabPanel[index].classList.add('tab-panels__tab-panel--active');
+    elsTabPanel[index].classList.add(modifieries.panelActive);
 
+  });
+});
+
+
+// Drop/Down Accordion
+elsAccordionToggler.forEach(function (elToggler, index) {
+  elToggler.addEventListener('click', function () {
+    elsAccordionToggler[index].classList.toggle(modifieries.accordionActive);
+    elsAccordionContent[index].classList.toggle(modifieries.toggleActive);
   });
 });
